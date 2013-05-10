147d52d5e7de757f67bb446a93c35f6c9e7b65ae
Chris Erlendson <cke2106@columbia.edu>
2013-05-10 15:39:13 -0400

    finished gain() and remainder() entropy functions, traverse() as well, next step is to figure out how to build the tree we would traverse.

--- Attribute.java
+++ Attribute.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 /**
  * Attribute class, for handling our attributes (e.g. Patrons, Wait time, etc.)
  * The value corresponding to an Attribute is a string
@@ -8,11 +10,33 @@
  *
  */
 public class Attribute {
+	/* numerical reference to attribute (4 refers to Patrons, for example) */
+	private int col; 
+	/* refers to value this attribute took on (e.g. "None", "Some", or "Full") */
 	private String value;
+	/* refers to what the class is; will mostly be null unless leaf node in tree */
+	private String classifier;
+	private Set<String> posValues;
+	
+	public Attribute()
+	{
+		
+	}
 	
 	public Attribute(String v)
 	{
 		value = v;
+		classifier = null;
+	}
+	
+	public int getCol()
+	{
+		return col;
+	}
+	
+	public void setCol(int c)
+	{
+		col = c;
 	}
 	
 	public String getValue()
@@ -24,6 +48,26 @@ public class Attribute {
 	{
 		value = v;
 		return value;
+	}
+	
+	public Set<String> getPosValues()
+	{
+		return posValues;
+	}
+	
+	public void setPosValues(Set<String> values)
+	{
+		posValues = values;
+	}
+	
+	public String getCls()
+	{
+		return classifier;
+	}
+	
+	public void setCls(String cls)
+	{
+		classifier = cls;
 	}
 	
 	public String toString()
--- DecisionTree.java
+++ DecisionTree.java
@@ -17,8 +17,13 @@ import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.Vector;
 
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 
 public class DecisionTree {
+
+	JTree root;
 
 	/**
 	 * 
@@ -29,9 +34,9 @@ public class DecisionTree {
 	 */
 	public float entropy(int total, Hashtable<String, Integer> classifiers)
 	{
-		System.out.println("Running entropy function...");
+		// System.out.println("Running entropy function...");
 		float total_entropy = 0;
-		System.out.println("total # of examples: " + total);
+		// System.out.println("total # of examples: " + total);
 
 		/* enumerate over all keys in examples (HashTable) */
 		for (Enumeration<String> e = classifiers.keys(); e.hasMoreElements();) {
@@ -41,24 +46,101 @@ public class DecisionTree {
 		}
 		return -total_entropy;
 	}
-	
+
+	/**
+	 * remainder() gives us the remaining entropy when focusing on a particular
+	 * attribute, thus taking the entropy of subsets based on value instead.
+	 *  
+	 * @param examples - given data, usually training_set or something
+	 * @param attr_values - values that an attribute can have
+	 * @param attr - which attribute do we want to focus on? i.e. 4 = Patrons
+	 * @return remaining entropy, usually smaller than total if given 
+	 * attribute divides up our original set nicely.
+	 */
 	public float remainder(	Vector<Vector<Attribute>> examples,
-							Vector<Set<String>> attr_values,
-							Hashtable<String, Integer> classifiers, int attr)
+			Vector<Set<String>> attr_values,
+			int attr)
 	{
 		float entropy = 0;
-		int total = examples.size();
-		Iterator<String> it = attr_values.get(attr).iterator();
-		
-		
-		
+		float total = examples.size();
+		int count;
+		Iterator<String> itValue = attr_values.get(attr).iterator();
+
+		/* for each value possible */
+		while(itValue.hasNext()) {
+			count = 0;
+			String value = itValue.next();
+			Iterator<Vector<Attribute>> itVector = examples.iterator();
+
+			/*keep a Hashtable to 
+			 * keep track of different classifiers within this subset, for
+			 * passing to our entropy function. Think the "Full" value of 
+			 * Patrons attribute, its mix of 2 positives and 3 negatives */
+			Hashtable<String, Integer> clrs = new Hashtable<String, Integer>();
+
+			/* how many examples had this value? */
+			while(itVector.hasNext()) {
+				Vector<Attribute> vec = itVector.next();
+				/* hit? */
+				if (vec.get(attr).toString().equals(value)) {
+					count++;
+					/* put it to our temporary Hashtable */
+					String cl = vec.lastElement().toString();
+					if (!clrs.containsKey(cl))
+						clrs.put(cl, 1);
+					else
+						clrs.put(cl, clrs.get(cl) + 1);
+
+				}
+
+			}
+			entropy += (count/total)*entropy(count, clrs);
+		}
+
 		return entropy;
 	}
-	
-	
-	
-	
-	
+
+	public float gain(	Hashtable<String, Integer> classifiers, 
+			Vector<Vector<Attribute>> examples,
+			Vector<Set<String>> attr_values,
+			int attr)
+	{
+		int totalSize = examples.size();
+		float initialEntropy = entropy(totalSize, classifiers);
+		float remainEntropy = remainder(examples, attr_values, attr);
+		return initialEntropy - remainEntropy;
+	}
+
+
+
+	public String traverse(Node<Attribute> node, Vector<Attribute> input)
+	{
+		/* leaf node? */
+		/* if the classifier is not null, then its a leaf, return classifier! */
+		if (node.data.getCls() != null)
+			return node.data.getCls();
+		else {
+			/* question node */
+			int att = node.data.getCol();
+			String v = input.get(att).getValue();
+
+			/* traverse children, e.g. which one has "Some"? */
+			Iterator<Node<Attribute>> itChild = node.children.iterator();
+			while(itChild.hasNext()) {
+				Node<Attribute> child = itChild.next();
+				if (child.getData().getValue().equals(v))
+					return traverse(child, input);
+			}
+			/* if we get here, throw exception: 
+			 * 	input had a value that we've never seen before */
+		}
+		return null;
+	}
+
+
+
+
+
 	/**
 	 * @param args - filename of csv file
 	 */
@@ -93,10 +175,14 @@ public class DecisionTree {
 					//display csv values
 					String value = st.nextToken();
 					input.add(new Attribute(value));
-					/* add the value to the corresponding attribute,
-					   if not already added */
+
+					/* for the first row, add to attr_values empty sets fore
+					 * every new attribute */
 					if (first_time)
 						attr_values.add(new HashSet());
+
+					/* add the value to the corresponding attribute,
+					 * if not already added */
 					attr_values.get(col).add(value);
 					col++;
 				}
@@ -118,24 +204,26 @@ public class DecisionTree {
 			System.out.println("Possible values for Attributes: " + attr_values.toString());
 
 			/* Entropy function for total training set here */
-//			float entropy = 0;
-//
-//			int total = training_set.size();
-//			System.out.println("total # of examples: " + total);
-//
-//			/* enumerate over all keys in classifiers (HashTable) */
-//			for (Enumeration<String> e = classifiers.keys(); e.hasMoreElements();) {
-//				/* c is the number of occurrences of the corresponding key */
-//				float c = classifiers.get(e.nextElement());
-//				entropy += (c/total)* Math.log(c/total)/Math.log(2);
-//			}
-//
-//			/* remember to negate */
-//			System.out.println("entropy: " + (-entropy));
+			//			float entropy = 0;
+			//
+			//			int total = training_set.size();
+			//			System.out.println("total # of examples: " + total);
+			//
+			//			/* enumerate over all keys in classifiers (HashTable) */
+			//			for (Enumeration<String> e = classifiers.keys(); e.hasMoreElements();) {
+			//				/* c is the number of occurrences of the corresponding key */
+			//				float c = classifiers.get(e.nextElement());
+			//				entropy += (c/total)* Math.log(c/total)/Math.log(2);
+			//			}
+			//
+			//			/* remember to negate */
+			//			System.out.println("entropy: " + (-entropy));
 
 			DecisionTree dt = new DecisionTree();
-			float entropy = dt.entropy(training_set.size(), classifiers);
-			System.out.println("entroppy: " + entropy);
+			float r_entropy = dt.remainder(training_set, attr_values, 4);
+			System.out.println("remaining entropy on Patrons: " + r_entropy);
+			float gain_entropy = dt.gain(classifiers, training_set, attr_values, 4);
+			System.out.println("gain entropy on Patrons: " + gain_entropy);
 
 
 		} catch(Exception e) {
